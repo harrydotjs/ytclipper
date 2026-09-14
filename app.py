b9c4b8456c3d09fcdccbb8d18200176bcfe13138
harrydotjs <138426723+harrydotjs@users.noreply.github.com>
2026-09-14 14:02:22 +0530

    Change yt_dlp extractor args for TV and iOS
    Updated extractor arguments for yt_dlp to support TV and iOS clients.

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ def download():
     command = [
         sys.executable, "-m", "yt_dlp",
         "--ffmpeg-location", ffmpeg_exe,
-        "--extractor-args", "youtube:player_client=android", 
+        "--extractor-args", "youtube:player_client=tv,ios", # Updated line        
         "--concurrent-fragments", "16",
         "--download-sections", f"*{start_time}-{end_time}",
         "-f", vid_format,
